7ac61c811a4f7a5eb1e01da2df8abfde8761d8e2
Prashant Vaibhav <prashant.vaibhav@tobyrich.com>
2017-05-29 12:44:53 +0200

    Replace dylib with shared framework
    Carthage only supports shared frameworks, so this changeset replaces
    the dynamic library target with a shared framework.

--- OSCKit/OSCKit.h
+++ OSCKit/OSCKit.h
@@ -1,3 +1,21 @@
-#import "OSCServer.h"
-#import "OSCClient.h"
-#import "OSCMessage.h"
+//
+//  OSCKit.h
+//  OSCKit
+//
+//  Created by Prashant Vaibhav on 29.05.17.
+//  Copyright © 2017 256dpi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OSCKit.
+FOUNDATION_EXPORT double OSCKitVersionNumber;
+
+//! Project version string for OSCKit.
+FOUNDATION_EXPORT const unsigned char OSCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSCKit/PublicHeader.h>
+
+#import <OSCKit/OSCServer.h>
+#import <OSCKit/OSCClient.h>
+#import <OSCKit/OSCMessage.h>
